dcb382e2949d6850e9c127821c72c88e8707cd60
ACER <ACER@172.16.120.114>
2025-10-11 09:00:40 +0700

    Nghia

--- Lab5/src/main/java/com/mycompany/lab5/bai1.java
+++ Lab5/src/main/java/com/mycompany/lab5/bai1.java
@@ -4,6 +4,9 @@
 
 package com.mycompany.lab5;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author ICT
@@ -11,6 +14,29 @@ package com.mycompany.lab5;
 public class bai1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ArrayList<Double> list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Nhập số thực: ");
+            Double x = scanner.nextDouble();
+            list.add(x);
+            scanner.nextLine(); // Đọc bỏ ký tự xuống dòng sau nextDouble
+
+            System.out.print("Nhập thêm (Y/N)? ");
+            String tiepTuc = scanner.nextLine();
+            if (tiepTuc.equalsIgnoreCase("N")) {
+                break;
+            }
+        }
+
+        System.out.println("\nDanh sách các số đã nhập:");
+        double tong = 0;
+        for (Double so : list) {
+            System.out.println(so);
+            tong += so;
+        }
+
+        System.out.println("Tổng các số: " + tong);
     }
 }
